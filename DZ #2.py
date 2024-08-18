3d8cfaf9697ec66dccd0283f6cfc94f21a4b0a35
YAbirow <wlad2468@gmail.com>
2024-08-19 01:43:37 +0300

    add file

--- DZ #2.py
+++ DZ #2.py
@@ -1,5 +1,4 @@
 a = "Python"
 rytm_count = 12
-giga_count = 0.125
 metsa_count = 15
 print(f"Курc: {a}, всего задач:{rytm_count}, затрачено часов:{metsa_count}, среднее время выполнения:{metsa_count/rytm_count} часа.")
