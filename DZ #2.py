e94b5dfd3842f398912949960f00b40b9baf7677
YAbirow <wlad2468@gmail.com>
2024-08-19 05:46:53 +0300

    add file

--- DZ #2.py
+++ DZ #2.py
@@ -1,4 +1,5 @@
 a = "Python"
 rytm_count = 12
-metsa_count = 15
-print(f"Курc: {a}, всего задач:{rytm_count}, затрачено часов:{metsa_count}, среднее время выполнения:{metsa_count/rytm_count} часа.")
+metsa_count = 1.5
+count=metsa_count/rytm_count
+print(f"Курc: {a}, всего задач:{rytm_count}, затрачено часов:{metsa_count}, среднее время выполнения:{count} часа.")
