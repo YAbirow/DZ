7a59265173067e9af4952393874e0ec4ce85123e
YAbirow <wlad2468@gmail.com>
2024-08-24 21:24:07 +0300

    add file

--- DZ #3.py
+++ DZ #3.py
@@ -1,8 +1,7 @@
 name = "Владислав"
-age = "21"
-new_age = "22"
+age = 21
 is_student = True
 print(f"Name: {name}")
 print(f"Age: {age}")
-print(f"New Age: {new_age}")
+print(f"New Age: {age+1}")
 print(f"Is Student: {is_student}")
