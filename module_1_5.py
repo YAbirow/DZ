e925a6fe3205315213f3da6b6d5203b90331cfba
YAbirow <wlad2468@gmail.com>
2024-09-01 21:39:47 +0300

    add file

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var = (1,2,3,41,'a','b','c','A','B','C','Test')
 #immutable_var[0] = 0    кортеж относится к неизменяемым типам данных
-mutable_list = ('q','w','x','y','z',[1,2,3])
+mutable_list = ['q','w','x','y','z',[1,2,3]]
 mutable_list[5][2] = 5
 
 
