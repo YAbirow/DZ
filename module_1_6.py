bde686b4827c9859d19285a8ee3dff3aa0234ccd
YAbirow <wlad2468@gmail.com>
2024-09-06 23:42:47 +0300

    add file

--- module_1_6.py
+++ module_1_6.py
@@ -1,11 +1,13 @@
-my_dict = {"Vlad",2003}
+my_dict = {"Vlad":2003}
 print(my_dict)
-print(my_dict.pop())
-my_dict.update({"Vlad",2003,
-                "Ignat",2004,
-                "Igor",2005})
+print(my_dict.pop("Vlad"))
+print(my_dict.get("inton"), "Такой переменной нет")
+
+my_dict.update({"Ignat":2004,
+                "Igor":2005})
+
 print(my_dict)
-print(my_dict.clear())
+
 
 my_set = {1,2,3,4,5,32421}
 print(my_set)
